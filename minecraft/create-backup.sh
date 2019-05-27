e21c63f3d5ecc7bd75b44ed8619fd26c9b3cfd58
p0rt23 <msmith4@gmail.com>
2019-05-27 11:18:01 -0400

    Making backups on the fly

--- minecraft/create-backup.sh
+++ minecraft/create-backup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Get rid of backups older than 7 days:
 find /opt/minecraft/backups -type f -mtime +7 -name '*.gz' -exec rm -- '{}' \;
 
-/opt/minecraft/server-stop.sh
+# Turn off server autosaving, and save all data:
+tmux send-keys -t minecraft 'say Backup process starting...' C-m 'save-off' C-m 'save-all' C-m
+sleep 5
 
 tar -czvf /opt/minecraft/backups/world_$(date '+%Y-%m-%d_%H-%M-%S').tar.gz /opt/minecraft/world
 
-/opt/minecraft/server-start.sh
+# Turn server autosaving back on:
+tmux send-keys -t minecraft 'save-on' C-m 'say Backup complete!' C-m 
